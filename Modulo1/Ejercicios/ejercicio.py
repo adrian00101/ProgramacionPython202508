cf4146a11528f1b22869fab32c443967dd3f1ea2
adrian00101 <alcalaadrian634@gmail.com>
2025-09-06 23:56:50 +0000

    cambios clase 1

---
+++ Modulo1/Ejercicios/ejercicio.py
@@ -0,0 +1,12 @@
+"""cree un programa que permita ingresar los siguientes datos de una persona:
+nombre, apellido, edad
+
+Realize la suma de edad+10
+"""
+#ingreso de datos
+nombre_persona=input("ingrese su nombre:")
+apellido_persona = input("ingrese su apellido:")
+edad_persona = input("coloque su edad:")
+edad_dentro_de_10_años = int(edad_persona)+10
+
+print(f)
